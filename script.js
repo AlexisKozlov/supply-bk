ea4e916a227d3a57359c96884c296f92fd8eced2
Alexis Kozlov <71926607+AlexisKozlov@users.noreply.github.com>
2025-03-11 10:04:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ const cardDatabase = {
         name: "Картофель \"фри\" 12,5 кг",
         analogs: ["FMS9", "58542", "68544", "555555"]
     }
-};
  "69889": {
         name: "Лук красный марин. закус заморож. 6 уп * 500 г",
         analogs: ["69890"]
     },
+};
 
 function searchCard() {
     let input = document.getElementById("searchInput").value.trim();
