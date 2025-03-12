2d5b6968a611c666acdfc3d13cc7e5a5a7926b10
Alexis Kozlov <71926607+AlexisKozlov@users.noreply.github.com>
2025-03-12 16:54:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ function searchCard() {
         let output = foundCards.map(card => `<h3 style='margin-bottom: 5px;'>${card.article} ${card.name}</h3>`).join("");
         resultElement.innerHTML = output;
     } else {
-        resultElement.innerHTML = "<p>Карточка не найдена, возможно она актуальна!</p>";
+        resultElement.innerHTML = "<p>Карточка не найдена, возможно она не имеет аналогов!</p>";
     }
 }
 
