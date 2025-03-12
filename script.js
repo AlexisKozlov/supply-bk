16467d1240d0ea2acb6960e0fe56b5fd8116d04a
Alexis Kozlov <71926607+AlexisKozlov@users.noreply.github.com>
2025-03-12 09:45:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ for (let key in cardDatabase) {
 }
 
     if (foundCards.length > 0) {
-        let output = foundCards.map(card => `<h3>${card.article} ${card.name}</h3>`).join("<br>");
+        let output = foundCards.map(card => `${card.article} ${card.name}>`).join("<br>");
         resultElement.innerHTML = output;
     } else {
         resultElement.innerHTML = "<p>Артикул не найден, возможно у карточки нет аналогов</p>";
