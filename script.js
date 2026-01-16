1d1f1fbc81f8d77466bb4a430c27143452173f4e
Alexis Kozlov <71926607+AlexisKozlov@users.noreply.github.com>
2026-01-16 10:10:55 +0000

    add changes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ if (typeof cardDatabase === 'undefined') {
 
 const AppConfig = {
     version: "1.2.1",
-    lastUpdate: "16.01.2026",
+    lastUpdate: "17.01.2026",
     maintenanceMode: false,
     adminPassword: "157"
 };
