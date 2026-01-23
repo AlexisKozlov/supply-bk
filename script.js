724d16287370080abff8e39654783b6589ff248c
Alexis Kozlov <71926607+AlexisKozlov@users.noreply.github.com>
2026-01-23 15:51:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://obywcpilionribalfrbl.supabase.co";
 const SUPABASE_KEY = "sb_publishable_BYToHeprZE-e64UjDgjlmQ_bKZBUFJ0";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 let cardDatabase = {};
 
